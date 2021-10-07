7b4c22c0d655f9345071ca3e02a32c7a7e5f8ce8
soniadotcom <sofiykafedor@gmail.com>
2021-10-08 00:04:05 +0300

    Add project files.

---
+++ ChessModel/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleChessApp;
+using System;
+
+namespace ConsoleChessApp
+{
+    public class Program
+    {
+        static Board myBoard = new Board(8);
+        static void Main(string[] args)
+        {
+            printBoard(myBoard);
+
+
+            Console.ReadLine();
+        }
+
+        private static void printBoard(Board myBoard)
+        {
+            
+
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    Cell c = myBoard.theGrid[i, j];
+
+                    if (c.CurrentlyOccupied == true)
+                    {
+                        Console.Write("X");
+                    }
+                    else if (c.LegalNextMove == true)
+                    {
+                        Console.Write("+");
+
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+            }
+            Console.WriteLine("========================================");
+        }
+    }
+}
---
+++ ConsoleChessApp/Board.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleChessApp
+{
+    public class Board
+    {
+
+        public int Size { get; set; }
+
+        public Cell[,] theGrid { get; set; }
+        public Board (int s)
+        {
+            Size = s;
+
+            theGrid = new Cell[Size, Size];
+            for (int i=0; i< Size; i++)
+            {
+                theGrid[i, j] = new Cell(i, j);
+            }
+        }
+
+
+        public void MarkLegalMoves(Cell currentCell, string chessPiece)
+        {
+            for (int i=0; i < Size; i++)
+            {
+                for (int j=0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+            switch (chessPiece)
+            {
+                case "Knight":
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColNumber - 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColNumber - 2].LegalNextMove = true;
+                    break;
+                case "King":
+
+                    break;
+                case "Rook":
+
+                    break;
+                case "Bishop":
+
+                    break;
+                case "Queen":
+
+                    break;
+                case "Quoridor":
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColNumber].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColNumber].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber, currentCell.ColNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber, currentCell.ColNumber - 1].LegalNextMove = true;
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+}
