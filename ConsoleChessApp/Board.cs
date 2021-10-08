489bc0c19f953cb4c48712b48c2cda3de7d6c548
VladySlave-P <vlad.polishuk.007@gmail.com>
2021-10-08 18:13:04 +0300

    add Player

--- ConsoleChessApp/Board.cs
+++ ConsoleChessApp/Board.cs
@@ -10,63 +10,79 @@ namespace ConsoleChessApp
         public int Size { get; set; }
 
         public Cell[,] theGrid { get; set; }
-        public Board (int s)
+        public Board(int s)
         {
             Size = s;
 
             theGrid = new Cell[Size, Size];
-            for (int i=0; i< Size; i++)
+            for (int i = 0; i < Size; i++)
             {
-                theGrid[i, j] = new Cell(i, j);
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
             }
         }
 
 
-        public void MarkLegalMoves(Cell currentCell, string chessPiece)
+        public void MarkLegalMoves(Player player)
         {
-            for (int i=0; i < Size; i++)
+            ClearLegalMoves();
+
+            if (isSave(player.Cell.RowNumber + 1, player.Cell.ColNumber))
+                if (theGrid[player.Cell.RowNumber + 1, player.Cell.ColNumber].CurrentlyOccupied)
+                {
+                    if (isSave(player.Cell.RowNumber + 2, player.Cell.ColNumber))
+                        theGrid[player.Cell.RowNumber + 2, player.Cell.ColNumber].LegalNextMove = true;
+                }
+                else
+                    theGrid[player.Cell.RowNumber + 1, player.Cell.ColNumber].LegalNextMove = true;
+
+            if (isSave(player.Cell.RowNumber - 1, player.Cell.ColNumber))
+                if (theGrid[player.Cell.RowNumber - 1, player.Cell.ColNumber].CurrentlyOccupied)
+                {
+                    if (isSave(player.Cell.RowNumber - 2, player.Cell.ColNumber))
+                        theGrid[player.Cell.RowNumber - 2, player.Cell.ColNumber].LegalNextMove = true;
+                }
+                else
+                    theGrid[player.Cell.RowNumber - 1, player.Cell.ColNumber].LegalNextMove = true;
+
+            if (isSave(player.Cell.RowNumber, player.Cell.ColNumber + 1))
+                if (theGrid[player.Cell.RowNumber, player.Cell.ColNumber + 1].CurrentlyOccupied)
+                {
+                    if (isSave(player.Cell.RowNumber, player.Cell.ColNumber + 2))
+                        theGrid[player.Cell.RowNumber, player.Cell.ColNumber + 2].LegalNextMove = true;
+                }
+                else
+                    theGrid[player.Cell.RowNumber, player.Cell.ColNumber + 1].LegalNextMove = true;
+
+            if (isSave(player.Cell.RowNumber, player.Cell.ColNumber - 1))
+                if (theGrid[player.Cell.RowNumber, player.Cell.ColNumber - 1].CurrentlyOccupied)
+                {
+                    if (isSave(player.Cell.RowNumber, player.Cell.ColNumber - 2))
+                        theGrid[player.Cell.RowNumber, player.Cell.ColNumber - 2].LegalNextMove = true;
+                }
+                else
+                    theGrid[player.Cell.RowNumber, player.Cell.ColNumber - 1].LegalNextMove = true;
+
+
+            theGrid[player.Cell.RowNumber, player.Cell.ColNumber].CurrentlyOccupied = true;
+        }
+
+        public void ClearLegalMoves()
+        {
+            for (int i = 0; i < Size; i++)
             {
-                for (int j=0; j < Size; j++)
+                for (int j = 0; j < Size; j++)
                 {
                     theGrid[i, j].LegalNextMove = false;
-                    theGrid[i, j].CurrentlyOccupied = false;
                 }
             }
+        }
 
-            switch (chessPiece)
-            {
-                case "Knight":
-                    theGrid[currentCell.RowNumber + 2, currentCell.ColNumber + 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 2, currentCell.ColNumber - 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 2, currentCell.ColNumber + 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 2, currentCell.ColNumber - 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 1, currentCell.ColNumber + 2].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber + 1, currentCell.ColNumber - 2].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 1, currentCell.ColNumber + 2].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 1, currentCell.ColNumber - 2].LegalNextMove = true;
-                    break;
-                case "King":
-
-                    break;
-                case "Rook":
-
-                    break;
-                case "Bishop":
-
-                    break;
-                case "Queen":
-
-                    break;
-                case "Quoridor":
-                    theGrid[currentCell.RowNumber + 1, currentCell.ColNumber].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber - 1, currentCell.ColNumber].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber, currentCell.ColNumber + 1].LegalNextMove = true;
-                    theGrid[currentCell.RowNumber, currentCell.ColNumber - 1].LegalNextMove = true;
-                    break;
-                default:
-
-                    break;
-            }
+        public bool isSave(int rowNumber, int colNumber)
+        {
+            return rowNumber >= 0 && rowNumber < 9 && colNumber >= 0 && colNumber < 9;
         }
     }
 }
